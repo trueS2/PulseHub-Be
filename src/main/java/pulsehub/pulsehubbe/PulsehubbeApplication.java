79f15fc83b22387420ea007a7458eb6da586b8f9
true <truee030@gmail.com>
2025-05-16 15:10:00 +0900

    chore: 초기 세팅 및 환경 구성

---
+++ src/main/java/pulsehub/pulsehubbe/PulsehubbeApplication.java
@@ -0,0 +1,13 @@
+package pulsehub.pulsehubbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsehubbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsehubbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pulsehub/pulsehubbe/PulsehubbeApplicationTests.java
@@ -0,0 +1,13 @@
+package pulsehub.pulsehubbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsehubbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
